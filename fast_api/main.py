5ba2e6ec750616fa6e427bfa35e3f77ceed7b3ee
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2024-02-06 11:51:35 +0100

    CROS origins: support local server

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,28 +1,50 @@
-from fastapi import FastAPI
-from enum import Enum
-import io
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
 import json
-from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
 import cv2
-import numpy as np
-from PIL import Image
 from ultralytics import YOLO
-from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image import convert_from_path
 from pathlib import Path
-import shutil
 import uvicorn
-import re
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # store uploaded images temporary folder
 UPLOAD_DIRECTORY = Path("/static/uploads")
 UPLOAD_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
 
-# Get the predicted values from json
-def find_value(detections_res: [], key: str):
+def check_confidence(label_conf):
+    high_conf_drawings = []
+    low_conf_drawings = []
+
+    for label, conf in label_conf:
+        if conf > 0.60:
+            high_conf_drawings.append(label)
+
+        # Decide how to handle drawings with lower confidence
+        elif 0.20 < conf < 0.59:
+            low_conf_drawings.append([label, conf])
+
+    return high_conf_drawings, low_conf_drawings
+
+
+# Get the predicted key-value pairs from json
+def find_value(detections_res: str, drawing_name: str, conf: str):
     try:
         json_data = json.loads(detections_res)
     except json.JSONDecodeError as e:
@@ -30,43 +52,62 @@ def find_value(detections_res: [], key: str):
         return None
 
     if isinstance(json_data, list):
+        labelname_conf_combined = []
+
         for item in json_data:
-            if key in item:
-                value = item[key]
+            temp = []
+            if drawing_name in item:
+                value = item[drawing_name]
+                temp.append(value)
+            if conf in item:
+                value = item[conf]
                 if isinstance(value, (int, float)):
-                    print(f"{key} value: {value}, {round(float(value), 2)}")
-                    return round(float(value), 2)
-                else:
-                    print(f"{key} value: {value}")
-                    return str(value)
-        print(f"{key} not found in any item of the JSON.")
-        return None
+                    temp.append(round(float(value), 2))
+            labelname_conf_combined.append(temp)
+
+        return labelname_conf_combined
+
     else:
         print("Invalid JSON format or not a list.")
         return None
 
-
 def check_detections(detections_json):
-    # Return message if no detections or if conf. < some value
-    if find_value(detections_json[0], "name") is None or find_value(detections_json[0],"confidence") < 0.60:
+    # Return message if no detections
+    if len(find_value(detections_json[0], "name", "confidence")) == 0:
         return {"Er du sikker på at dette er riktig tegning?"}
 
     else:
-        drawing_type=find_value(detections_json[0],"name")
-        return drawing_type
+        label_conf_combined=find_value(detections_json[0], "name", "confidence")
+        # check if confidence is above a threshold value
+        high_conf_drawings, low_conf_drawings = check_confidence(label_conf_combined)
+        return high_conf_drawings
+
+
+@app.get("/")
+async def root():
+    return JSONResponse({"message": "Hello World!!"})
+
 @app.post("/detect/")
 async def detect_objects (uploaded_file: UploadFile = File(...)):
-    model = YOLO("runs/detect/train/weights/best.pt")
+    model_path = r"../runs/detect/train/weights/best.pt"
+
+    if os.path.exists(model_path):
+        print("File exists.")
+    else:
+        print("File does not exist.")
+
+    model = YOLO(model_path)
     # Process the uploaded image for object detection
     file_path = UPLOAD_DIRECTORY/uploaded_file.filename
 
     # store uploaded file in temp folder
-    with open(file_path,"wb") as file_object:
+    with open(file_path, "wb") as file_object:
         # read file into memory in bytes
         file_object.write(uploaded_file.file.read())
 
     detections_json = []
 
+    print(uploaded_file.filename.lower().endswith('.pdf'))
     # convert file to image if pdf
     if uploaded_file.filename.lower().endswith('.pdf'):
         input_images = convert_from_path(file_path)
@@ -78,7 +119,7 @@ async def detect_objects (uploaded_file: UploadFile = File(...)):
                 detections_json.append(detections)
 
     # read file directly as an image from folder
-    elif uploaded_file.filename.lower().endswith(('.jpg','.jpeg', '.png')):
+    elif uploaded_file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         image = cv2.imread(str(file_path))
         results = model.predict(image)
         for r in results:
@@ -88,7 +129,9 @@ async def detect_objects (uploaded_file: UploadFile = File(...)):
     # Check detections in drawing and return message
     drawing_check = check_detections(detections_json)
 
+    print(drawing_check)
+
     return {"message": drawing_check}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- fast_api/test_main.py
+++ fast_api/test_main.py
@@ -2,12 +2,18 @@ from fastapi.testclient import TestClient
 
 from main import app
 import os
+
 client = TestClient(app)
 
 
 def test_detect_objects_with_image ():
     # Path to a test image file
-    test_image_path = 'fast_api/static/uploads/E1-Plantegning%20U.etg%20-%20bruksendring.jpg'
+    test_image_path = 'static/uploads/2Enebolig_page_1.jpg'
+
+    if os.path.exists(test_image_path):
+        print("File exists.")
+    else:
+        print("File does not exist.")
 
     # Open the test image file in binary mode
     with open(test_image_path, 'rb') as test_image:
@@ -25,13 +31,13 @@ def test_detect_objects_with_image ():
     assert len(detections) > 0  # Example check: ensure at least one detection was made
 
 
-def test_detect_objects_with_pdf ():
+#def test_detect_objects_with_pdf ():
     # Similar structure as the image test, but with a PDF file
-    test_pdf_path = 'fast_api/static/uploads/Tegning%20ny%20fasade.PDF'
+#    test_pdf_path = 'static/uploads/Tegning20ny20fasade.PDF'
 
-    with open(test_pdf_path, 'rb') as test_pdf:
-        files = {'uploaded_file': (os.path.basename(test_pdf_path), test_pdf, 'application/pdf')}
-        response = client.post("/detect/", files=files)
+#    with open(test_pdf_path, 'rb') as test_pdf:
+#        files = {'uploaded_file': (os.path.basename(test_pdf_path), test_pdf, 'application/pdf')}
+#        response = client.post("/detect/", files=files)
 
-    assert response.status_code == 200
-    # Add assertions to verify PDF handling
+#    assert response.status_code == 200
+#    # Add assertions to verify PDF handling
