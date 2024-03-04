47eadac66f8279264e8d07fe4c220f5fa4f92e31
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2024-03-04 10:13:13 +0100

    deleted a img

--- fast_api/json_response_converter.py
+++ fast_api/json_response_converter.py
@@ -4,10 +4,12 @@ from models import Detection
 
 def json_response_converter(detection_response: List[Detection]) -> list:
     response = []
-    drawing_types = []
+    #drawing_types = []
     for file in detection_response:
-        obj = {}
-
+        obj = {
+            'file_name': file['file_name']
+        }
+        drawing_types = []
         if not file['drawing_types']:
             obj = {
                 **obj,
@@ -17,16 +19,17 @@ def json_response_converter(detection_response: List[Detection]) -> list:
             for drawing_type in file['drawing_types']:
                 if drawing_type not in drawing_types:
                     drawing_types.append(drawing_type)
-
+            obj = {
+                **obj,
+                'drawing_type': drawing_types
+            }
         for key in ['scale', 'room_names', 'cardinal_direction']:
             if file.get(key):
                 obj[key] = file[key]
 
         if bool(obj):
-            response.append({
-                file['file_name']: obj
-            })
+            response.append(obj)
 
-    for key in drawing_types:
-        response.append(key)
+    #for key in drawing_types:
+    #    response.append(key)
     return response
