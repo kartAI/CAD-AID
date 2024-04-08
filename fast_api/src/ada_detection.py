e56fee5eae31039a391d2bd7426f92c3cf5df6b3
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2024-04-08 14:39:18 +0200

    some minor changes

--- fast_api/src/ada_detection.py
+++ fast_api/src/ada_detection.py
@@ -9,13 +9,13 @@ def ada_detection(image: MatLike, file_types):
     reader = easyocr.Reader(['no'], gpu=False)
     results = reader.readtext(image, width_ths=0.7)
 
-    decoded_labels = []
-    decoded_position = []
+    detected_text = []
+    detected_text_coordinates = []
 
     for result in results:
         bbox,text,prob = result
-        decoded_labels.append(text)
-        decoded_position.append(bbox)
+        detected_text.append(text)
+        detected_text_coordinates.append(bbox)
 
     conditions = {
         'fasade': {
@@ -31,7 +31,7 @@ def ada_detection(image: MatLike, file_types):
         for drawing_type, sub_conditions in conditions.items():
             if drawing_type == file_type:
                 for condition, (pattern, message) in sub_conditions.items():
-                    if not any(regex.search(pattern, ocr_label.lower()) for ocr_label in decoded_labels):
+                    if not any(regex.search(pattern, ocr_label.lower()) for ocr_label in detected_text):
                         obj[condition] = message
 
-    return obj, decoded_labels, decoded_position
+    return obj, detected_text, detected_text_coordinates
--- fast_api/src/main.py
+++ fast_api/src/main.py
@@ -49,8 +49,8 @@ async def detect_objects(uploaded_files: List[UploadFile]):
                 nora: list = nora_detection(image)
 
                 if 'fasade' in nora or 'plantegning' in nora:
-                    ada, detected_text, decoded_position = ada_detection(image, nora)
-                    eva = eva_segmentation(image, detected_text, decoded_position)
+                    ada, detected_text, detected_text_coordinates = ada_detection(image, nora)
+                    eva = eva_segmentation(image, detected_text, detected_text_coordinates)
 
             detection_response.append({
                 'drawing_types': nora,
@@ -66,8 +66,8 @@ async def detect_objects(uploaded_files: List[UploadFile]):
             nora: list = nora_detection(image)
 
             if 'fasade' in nora or 'plantegning' in nora:
-                ada, detected_text, decoded_position = ada_detection(image, nora)
-                eva = eva_segmentation(image, detected_text, decoded_position)
+                ada, detected_text, detected_text_coordinates = ada_detection(image, nora)
+                eva = eva_segmentation(image, detected_text, detected_text_coordinates)
 
             detection_response.append({
                 'drawing_types': nora,
