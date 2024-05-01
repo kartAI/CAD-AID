6c2bfe08e048c1d4915effb87d8ce3d27f4c7757
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2024-05-01 09:44:55 +0200

    pytest, some refactoring, support files in wrong format

--- fast_api/src/main.py
+++ fast_api/src/main.py
@@ -2,7 +2,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import os
 from concurrent.futures import ThreadPoolExecutor
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException, status
 from pathlib import Path
 from pdf2image import convert_from_path
 from typing import List
@@ -10,6 +10,10 @@ from .nora_detection import nora_detection
 from .ada_detection import ada_detection
 from .eva_segmentation import eva_segmentation
 from .json_response_converter import json_response_converter
+#from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+import time
+
 app = FastAPI()
 
 origins = [
@@ -28,12 +32,12 @@ app.add_middleware(
 UPLOAD_DIRECTORY = Path("static/uploads")
 UPLOAD_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
-def detect_and_validate(image, uploaded_file): 
+def detect_and_validate(image, uploaded_file):
     nora: list = nora_detection(image)
 
     ada = {}
     eva = {}
-    
+
     if 'fasade' in nora or 'plantegning' in nora:
         ada, detected_text, detected_text_coordinates = ada_detection(image, nora)
         if 'plantegning' in nora:
@@ -63,10 +67,33 @@ def process_file(uploaded_file):
         detection_response.append(detect_and_validate(image, uploaded_file))
 
     os.remove(file_path)
-    return detection_response[0]
+    if len(detection_response) > 0:
+        return detection_response[0]
+    return {}
 
 @app.post("/detect/")
 async def detect_objects(uploaded_files: List[UploadFile]):
+
+    # FILE_SIZE = 26214400 # 25 * 1024 * 1024 = 25MB
+    # real_file_size = 0
+
+    # for file in uploaded_files:
+    #     for chunk in file.file:
+    #         real_file_size += len(chunk)
+    #         if real_file_size > FILE_SIZE:
+    #             raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, detail="Too large files")
+          
+    # start_time = time.time()           
+    # num_cores = multiprocessing.cpu_count()
+
+    # with ProcessPoolExecutor(max_workers=num_cores) as executor:
+    #     res = executor.map(process_file, uploaded_files)
+    #     print(res)
+    
+    # end_time = time.time()
+    # elapsed_time = end_time - start_time
+    # print("Elapsed Time:", elapsed_time)
+    # return []
     with ThreadPoolExecutor() as executor:
-     
+
         return json_response_converter(executor.map(process_file, uploaded_files))
--- fast_api/src/test_main.py
+++ fast_api/src/test_main.py
@@ -1,71 +1,68 @@
 from fastapi.testclient import TestClient
 
-from .main import app
+from main import app
 import os
-import pytest
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError, URLError
-import json
-import os
-
-
-# The base URL of the FastAPI server
-BASE_URL = "http://127.0.0.1:8080"
-
-# The endpoint to test
-ENDPOINT = "/detect/"
-
-
-
-def make_request_to_documents():
-    """Make a request to the /documents/ endpoint."""
-    request = Request(f"{BASE_URL}{ENDPOINT}")
-    request.data()
-
-    try:
-        response = urlopen(request)
-        data = response.read().decode("utf-8")
-        return json.loads(data)
-    except HTTPError as e:
-        return e.code
-    except URLError as e:
-        return e.reason
 
 client = TestClient(app)
 
 
 def test_detect_objects_with_image():
     # Path to a test image file
-    test_image_path = 'static/uploads/2Enebolig_page_1.jpg'
+    test_image_path = 'test_images/situasjonskart.jpg'
 
     if os.path.exists(test_image_path):
         print("File exists.")
     else:
         print("File does not exist.")
 
-    # Open the test image file in binary mode
-    with open(test_image_path, 'rb') as test_image:
-        # Create a dict to simulate form data_old with a file upload
-        files = {'uploaded_file': (os.path.basename(test_image_path), test_image, 'image/jpeg')}
 
-        # Send a POST request to the endpoint with the test image
-        response = client.post("/detect/", files=files)
+    files = [('uploaded_files', (os.path.basename(test_image_path), open(test_image_path, 'rb'), 'image/jpeg'))]
+    response = client.post("/detect/", files=files)
 
-    # Verify the status code of the response
+    assert response.status_code == 200
+    assert response.json()[0]['file_name'] == 'situasjonskart.jpg'
+    assert response.json()[0]['drawing_type'] == ['situasjonskart']
+
+
+def test_upload_multible_files():
+    test_image_paths= ['test_images/situasjonskart.jpg', 'test_images/snitt.jpg']
+
+    multiple_files = [('uploaded_files', (os.path.basename(test_image_paths[0]), open(test_image_paths[0], 'rb'), 'image/jpeg')),
+                      ('uploaded_files', (os.path.basename(test_image_paths[1]), open(test_image_paths[0], 'rb'), 'image/jpeg'))]
+
+    response = client.post("/detect/", files=multiple_files)
+
     assert response.status_code == 200
 
-    # Parse the response JSON and perform further checks as needed
-    detections = response.json()['detections']
-    assert len(detections) > 0  # Example check: ensure at least one detection was made
 
+# def test_maxsize():
+#     test_image_path = 'test_images/CAD-aid-DEMO.mp4' 
 
-#def test_detect_objects_with_pdf ():
+#     multiple_files = [('uploaded_files', (os.path.basename(test_image_path), open(test_image_path, 'rb'), 'video/mp4')),
+#                       ('uploaded_files', (os.path.basename(test_image_path), open(test_image_path, 'rb'), 'video/mp4')),
+#                       ('uploaded_files', (os.path.basename(test_image_path), open(test_image_path, 'rb'), 'video/mp4')),
+#                       ('uploaded_files', (os.path.basename(test_image_path), open(test_image_path, 'rb'), 'video/mp4')),
+#                     ]
+
+#     response = client.post("/detect/", files=multiple_files)
+
+#     assert response.status_code == 413
+
+def test_not_acceptable_file_format():
+    test_image_path = 'test_images/CAD-aid-DEMO.mp4' 
+
+    multiple_files = [('uploaded_files', (os.path.basename(test_image_path), open(test_image_path, 'rb'), 'video/mp4'))]
+
+    response = client.post("/detect/", files=multiple_files)
+
+    assert response.json() == []
+
+def test_detect_objects_with_pdf ():
     # Similar structure as the image test, but with a PDF file
-#    test_pdf_path = 'static/uploads/Tegning20ny20fasade.PDF'
+   test_pdf_path = 'test_images/cad-aid-system-overview.pdf'
 
-#    with open(test_pdf_path, 'rb') as test_pdf:
-#        files = {'uploaded_file': (os.path.basename(test_pdf_path), test_pdf, 'application/pdf')}
-#        response = client.post("/detect/", files=files)
+   with open(test_pdf_path, 'rb') as test_pdf:
+       files = {'uploaded_files': (os.path.basename(test_pdf_path), test_pdf, 'application/pdf')}
+       response = client.post("/detect/", files=files)
 
-#    assert response.status_code == 200
-#    # Add assertions to verify PDF handling
+   assert response.status_code == 200
